30a27da65153e620e5548993183c44c6977bbc80
Diego Curtino <dcurtino@gmail.com>
2021-05-04 18:54:16 +0200

    Add home page, navigation bar & simple animations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
+import { HomeComponent } from './home/home.component';
 import { LoanQuoteFormComponent } from './loan-quote-form/loan-quote-form.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'loanquote', component : LoanQuoteFormComponent }
+  { path: 'loanquote', 
+    component : LoanQuoteFormComponent
+  },
+  { path: '', 
+    component : HomeComponent
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { LoanQuoteService } from './loan-quote-service/loan-quote.service';
 import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoanQuoteFormComponent
+    LoanQuoteFormComponent,
+    HomeComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule, 
